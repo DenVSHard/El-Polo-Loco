3a7cdd1d73f262f3d403abf72c7dc40181ecf86e
DenVSHard <denisweinhardt@icloud.com>
2023-10-25 23:25:16 +0200

    aktualisierung

--- classes/character.class.js
+++ classes/character.class.js
@@ -4,6 +4,7 @@ class Character extends MovableObject {
     img;
     height = 250;
     width = 130;
+    speed = 4;
     IMAGES_WALKING = [
         'img/img/2_character_pepe/2_walk/W-21.png',
         'img/img/2_character_pepe/2_walk/W-22.png',
@@ -22,15 +23,27 @@ class Character extends MovableObject {
     }
 
     animate() { // durchläuft das array IMAGES_WALKING
-        setInterval(() => { // animiert den character
-
+        setInterval(() => {
             if (this.world.keyboard.RIGHT) {
+                this.x += this.speed;
+                this.otherDirection = false;
+            }
+
+            if (this.world.keyboard.LEFT) {
+                this.x -= this.speed;
+                this.otherDirection = true;
+            }
+            this.world.camera_x = -this.x; // bewegt die camera mit und fexirt sie auf den charecter
+        }, 1000 / 60);
+
+        setInterval(() => { // animiert den character
+            if (this.world.keyboard.RIGHT || this.world.keyboard.LEFT) { // wenn pfeil nach rechts gedrückt wird
                 let i = this.currentImage % this.IMAGES_WALKING.length; // % ist ein modulo operator
                 let path = this.IMAGES_WALKING[i];
                 this.img = this.imageCache[path];
                 this.currentImage++; // springt zum nächsten bild
             }
-        }, 150); // alle 100 millisekunden
+        }, 50); // alle 100 millisekunden
     }
 
     jump() {
--- classes/world.class.js
+++ classes/world.class.js
@@ -1,22 +1,12 @@
 class World {
     character = new Character();
-    enemies = [
-        new Chicken(),
-        new Chicken(),
-        new Chicken(),
-    ];
-    backgroundObjects = [
-        new BackgroundObject('img/img/5_background/layers/air.png', 0),
-        new BackgroundObject('img/img/5_background/layers/3_third_layer/1.png', 0),
-        new BackgroundObject('img/img/5_background/layers/2_second_layer/1.png', 0),
-        new BackgroundObject('img/img/5_background/layers/1_first_layer/1.png', 0)
-    ];
-    clouds = [
-        new Cloud()
-    ];
+    enemies = level1.enemies;
+    clouds = level1.clouds;
+    backgroundObjects = level1.backgroundObjects;
     canvas;
     ctx; // abkürzung für context
     keyboard;
+    camera_x = 0;
 
     constructor(canvas, keyboard) {
         this.ctx = canvas.getContext('2d');
@@ -33,10 +23,14 @@ class World {
     draw() {
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height); //leert canvas
 
+        this.ctx.translate(this.camera_x, 0);
+
         this.addObjectsToMap(this.backgroundObjects);
         this.addToMap(this.character);
         this.addObjectsToMap(this.enemies);
         this.addObjectsToMap(this.clouds);
+
+        this.ctx.translate(-this.camera_x, 0);
 
         //wiederholt draw() immer wieder
         let self = this;
@@ -52,7 +46,18 @@ class World {
     }
 
     addToMap(MovableObject) {
+        // den character drehen
+        if (MovableObject.otherDirection) {
+            this.ctx.save();// context speichern
+            this.ctx.translate(MovableObject.width, 0); // img spiegeln
+            this.ctx.scale(-1, 1); // spiegelt den charecter um seine eigene breite
+            MovableObject.x = MovableObject.x * -1; // die x-coordinaten werden gespiegelt
+        }
         this.ctx.drawImage(MovableObject.img, MovableObject.x, MovableObject.y, MovableObject.width, MovableObject.height);
-
+        // den character zurück drehen
+        if (MovableObject.otherDirection) {
+            MovableObject.x = MovableObject.x * -1;
+            this.ctx.restore();
+        }
     }
 }
